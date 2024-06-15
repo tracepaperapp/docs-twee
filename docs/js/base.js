437af2f04912687ca2f919de1f68dc8459ae2fc3
bohanssen <bohanssen@gmail.com>
2024-06-15 12:13:17 +0200

    Update base.js

--- docs/js/base.js
+++ docs/js/base.js
@@ -69,7 +69,7 @@ function updateIframe(enableForwardNav) {
   // Grey out the "forward" button if we don't expect 'forward' to work.
   $('#hist-fwd').toggleClass('greybtn', !enableForwardNav);
 
-  console.log("BU",base_url,mainWindow.location.href,location.href);
+  console.log("BU",base_url,mainWindow.location.href,location.href); 
   var targetRelPath = getRelPath('#', mainWindow.location.href) || '';
   console.log("RP",targetRelPath);
   var targetIframeUrl = getAbsUrl('/', targetRelPath);
